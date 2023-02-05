4d1e5d98f28a86f54323cbf9ff51542a2193947a
Ynck-Hng <ynckhuang@gmail.com>
2023-02-05 21:28:34 +0100

    DOM - Starting to implement lists from here

--- frontend/js/listDOM.js
+++ frontend/js/listDOM.js
@@ -1,7 +1,6 @@
 const listTemplate = document.createElement("template");
 
 listTemplate.innerHTML = `
-
  <!-- Lists here -->
                 <article class="list__container" dataset-list-id="">
                     <div class="list__header">
@@ -9,15 +8,28 @@ listTemplate.innerHTML = `
                             <h2> Title list 1</h2>
                         </div>
                         
-                        <div class="list__header--delete">
-                            <a href="#"> x </a>
+                        
+                        <div class="list__buttons">
+                            <div class="list__header--create">
+                                <a href="#"> + </a>
+                            </div>
+                            <div class="list__header--delete">
+                                <a href="#"> x </a>
+                            </div>
                         </div>
                     </div>
 
                     <div class="cards__container">
                         
                     </div>
-                    <!-- End list -->
-                </article>
-
+                    </article>
+<!-- End list -->
 `
+const listClone = document.importNode(listTemplate.content, true);
+const listContainer = document.querySelector(".main__container--lists");
+const listDOM = {
+
+    insertListInHtml: (listData) => {
+
+    },
+}
