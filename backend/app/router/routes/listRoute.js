8faf8e90e2e16ef1a1d75c5dcfa0c4285efa6b11
Yannick Huang <115977341+Ynck-Hng@users.noreply.github.com>
2023-05-09 15:19:49 +0200

    Update listRoute.js

--- backend/app/router/routes/listRoute.js
+++ backend/app/router/routes/listRoute.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const listController = require("./../../controllers/listController");
+const bodySanitize = require("./../../middlewares/bodySanitizer");
 
 // Currently on route http://localhost:PORT/lists/
 
 router.get("/", listController.findAllLists);
-router.post("/", listController.createOnelist);
+router.post("/", bodySanitize, listController.createOnelist);
 router.get("/:listId", listController.findOneList);
-router.patch("/:listId", listController.updateOneList);
+router.patch("/:listId", bodySanitize, listController.updateOneList);
 router.delete("/:listId", listController.deleteOneList);
 
 module.exports = router;
